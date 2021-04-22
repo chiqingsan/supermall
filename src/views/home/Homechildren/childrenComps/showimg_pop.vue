3d7b481caa2a9cf5ecc27fa45457fabf30c8303b
chiqingsan <chiqingsan@gmail.com>
2021-04-23 01:49:29 +0800

    完成商品页

---
+++ src/views/home/Homechildren/childrenComps/showimg_pop.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="item in showimglist.pop.list">
+      <img :src="item.show.img" alt="">
+      <p></p>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "showimg",
+  props: {
+    showimglist: {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
